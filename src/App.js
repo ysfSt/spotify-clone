0701a03552aac41f51b0f0efe93a30e65b1f66af
YsfSt <ysfst2@gmail.com>
2021-02-05 22:42:56 +0100

    add main container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.scss";
 import SideBar from "./SideBar/SideBar";
+import Footer from "./Footer/Footer";
+import MainContainer from "./MainContainer/MainContainer";
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <MainContainer />
+      <Footer />
     </div>
   );
 }
---
+++ src/MainContainer/MainContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MainContainer.scss";
+
+const MainContainer = () => {
+  return (
+    <div className="mainContainer">
+      <h1>Main Container</h1>
+    </div>
+  );
+};
+
+export default MainContainer;

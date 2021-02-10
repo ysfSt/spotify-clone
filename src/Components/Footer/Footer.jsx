82b3caa66dad5a1ac4a177ba155de7e169362fb0
YsfSt <ysfst2@gmail.com>
2021-02-10 23:57:27 +0100

    add footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Footer.scss";
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousRoundedIcon from '@material-ui/icons/SkipPreviousRounded';
+import SkipNextRoundedIcon from '@material-ui/icons/SkipNextRounded';
+import ShuffleRoundedIcon from '@material-ui/icons/ShuffleRounded';
+import RepeatRoundedIcon from '@material-ui/icons/RepeatRounded';
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return <div className="footer">
+    <div className="footer__left">
+      {/* Album and song details */}
+    </div>
+    <div className="footer__center">
+      {/* Player Controls */}
+      <ShuffleRoundedIcon/>
+      <SkipPreviousRoundedIcon/>
+      <PlayCircleOutlineIcon/>
+      <SkipNextRoundedIcon/>
+      <RepeatRoundedIcon/>
+    </div>
+    <div className="footer__right">
+      {/* Volume Controls */}
+    </div>
+  </div>;
 };
 
 export default Footer;
